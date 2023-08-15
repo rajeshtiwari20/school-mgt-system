4c3853ee2c2e2ac4f7f890dcf16382fe2544756d
jitendra-pal <jitendrapal0009@gmail.com>
2023-08-15 21:01:57 +0530

    admin templet update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
+    Route::match (['get','post'],'login','AdminController@login');
+    Route::group(['middleware'=>['admin']],function(){
+        Route::get('dashboard','AdminController@dashboard');
+     });
+    
+});
+  
